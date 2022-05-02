cfc504d083e29e23abc28681b34988abb8b993ae
Andrew Owino <andystudy4@gmail.com>
2022-05-02 18:32:44 +0300

    Design layout

--- app/src/main/java/com/bambacode/mp3player/MainActivity.java
+++ app/src/main/java/com/bambacode/mp3player/MainActivity.java
@@ -1,14 +1,18 @@
 package com.bambacode.mp3player;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView playList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playList = findViewById(R.id.lv_playList);
+        playList.setAdapter(new ArrayAdapter<String>(this,));
     }
 }
