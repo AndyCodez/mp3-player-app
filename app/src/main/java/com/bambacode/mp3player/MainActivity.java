d18b92f0d5b2f57c1a6c84f460377090181eb211
Andrew Owino <andystudy4@gmail.com>
2022-05-02 19:49:22 +0300

    Request JSON data with Volley

--- app/src/main/java/com/bambacode/mp3player/MainActivity.java
+++ app/src/main/java/com/bambacode/mp3player/MainActivity.java
@@ -4,6 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     ListView playList;
@@ -13,6 +26,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         playList = findViewById(R.id.lv_playList);
-        playList.setAdapter(new ArrayAdapter<String>(this,));
+        // playList.setAdapter(new ArrayAdapter<String>(this,));
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://orangevalleycaa.org/api/music";
+
+        // Request a JsonArray response from the provided URL.
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+            JSONObject song;
+
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    song = response.getJSONObject(0);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(MainActivity.this, song.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Error occurred" + error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(request);
     }
 }
